7bfef147f56134d762bce167c50c70cb1dda9ef9
Parvin Sattorova <nanajon66@gmail.com>
2023-05-10 22:00:12 -0600

    feat: add functionallity that organizes guesses and records if they are correct or incorrect

---
+++ src/round.js
@@ -0,0 +1,37 @@
+const createRound = (deck) => ({
+    deck: deck,
+    currentCard: currentCard = deck[0],
+    incorrectGuesses: incorrectGuesses = [],
+    turns: turns = 0
+  });
+
+const takeTurn = (guess, round) => {
+  message = ''
+  if (guess === round.currentCard.correctAnswer) {
+    message = 'Correct!'
+  }else{
+    guess !== round.currentCard.correctAnswer;
+    message = 'Incorrect!'
+    round.incorrectGuesses.push(round.currentCard.id);
+  };
+  round.turns++
+  round.currentCard = round.deck[round.deck.indexOf(round.currentCard) + 1]
+  return message
+};
+
+const calculatePercentageCorrect = (round) => {
+  let percent = 1 - round.incorrectGuesses.length / round.turns
+  return percent * 100
+};
+  
+const endRound = (round) => {
+  let result = calculatePercentageCorrect(round)
+  return `** Round over! ** You answered ${result}% of the questions correctly!`
+};
+   
+  module.exports = {
+    createRound, 
+    takeTurn, 
+    calculatePercentageCorrect, 
+    endRound
+};
